65c91edec9e4db9f8dc7c191e2a6186557df6299
Husanboy <olimboyevhusanboy@gmail.com>
2022-11-30 10:12:27 +0500

    thank you

--- src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.task3;
 
 public class AliquotNumbers {
     public void isFirstAliquot(int first, int second) {
+        if ( second % first == 0) {
+            System.out.println("Aliquot");
+        }
+        else {
+            System.out.println("Not aliquot");
+        }
+      /*  if (second > 1) {
+            System.out.println("aliquot");
+        }
+        else {
+            System.out.println("not aliquot");
+        }*/
+
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,21 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        double result;
+        if (salary >= 0 && salary <= 10000) {
+            result = salary - salary * 15.0 / 100;
+            System.out.println(result);
+        }
+        if (salary > 10000 && salary <= 20000) {
+            result = salary - salary * 18.0 / 100;
+            System.out.println(result);
+        }
+        if (salary > 20000) {
+            result = salary - salary * 20.0 / 100;
+            System.out.println(result);
+        }
+        if (salary < 0) {
+            System.out.println("wrong input!");
+        }
     }
 }
